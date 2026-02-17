10039f10ec3964bd4a0475c5a84d5fe62a5e80e7
gpu-maker <32omelada@sandiaprep.org>
2026-02-17 14:51:58 -0700

    Create betting.js

---
+++ engine/betting.js
@@ -0,0 +1,41 @@
+class Player {
+  constructor(name,chips=1000){
+    this.name=name;
+    this.chips=chips;
+    this.bet=0;
+    this.hand=[];
+    this.folded=false;
+  }
+
+  placeBet(amount){
+    amount=Math.min(amount,this.chips);
+    this.chips-=amount;
+    this.bet+=amount;
+    return amount;
+  }
+
+  reset(){
+    this.bet=0;
+    this.hand=[];
+    this.folded=false;
+  }
+}
+
+class BettingRound{
+  constructor(players){
+    this.players=players;
+    this.pot=0;
+    this.currentBet=0;
+  }
+
+  bet(player,amount){
+    const paid=player.placeBet(amount);
+    this.pot+=paid;
+    this.currentBet=Math.max(this.currentBet,player.bet);
+  }
+
+  call(player){
+    const diff=this.currentBet-player.bet;
+    this.bet(player,diff);
+  }
+}
