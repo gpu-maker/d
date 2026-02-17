fc1aa878f0be5db19c681cad0be1f050164ed151
gpu-maker <32omelada@sandiaprep.org>
2026-02-17 14:52:57 -0700

    Create gameState.js

---
+++ engine/gameState.js
@@ -0,0 +1,23 @@
+const game={
+  deck:null,
+  community:[],
+  players:[],
+  betting:null,
+  stage:"preflop"
+};
+
+function startGame(){
+  game.deck=new Deck();
+  game.community=[];
+  game.players=[
+    new Player("You"),
+    new Player("AI")
+  ];
+
+  game.players.forEach(p=>{
+    p.reset();
+    p.hand=[game.deck.deal(),game.deck.deal()];
+  });
+
+  game.betting=new BettingRound(game.players);
+}
