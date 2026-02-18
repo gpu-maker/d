3f7d534fd71524ffea82ddf9d52097a277364b68
gpu-maker <32omelada@sandiaprep.org>
2026-02-18 11:50:20 -0700

    Update deck.js

--- engine/deck.js
+++ engine/deck.js
@@ -1,26 +1,23 @@
-const SUITS = ["♠","♥","♦","♣"];
-const VALUES = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+const SUITS=["♠","♥","♦","♣"];
+const VALUES=["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
 
-class Deck {
-  constructor() {
-    this.cards = [];
-    this.reset();
-  }
+class Deck{
+  constructor(){this.reset();}
 
-  reset() {
-    this.cards = [];
-    for (let s of SUITS)
-      for (let v of VALUES)
+  reset(){
+    this.cards=[];
+    for(let s of SUITS)
+      for(let v of VALUES)
         this.cards.push({suit:s,value:v});
     this.shuffle();
   }
 
-  shuffle() {
-    for (let i=this.cards.length-1;i>0;i--){
+  shuffle(){
+    for(let i=this.cards.length-1;i>0;i--){
       const j=Math.floor(Math.random()*(i+1));
       [this.cards[i],this.cards[j]]=[this.cards[j],this.cards[i]];
     }
   }
 
-  deal(){ return this.cards.pop(); }
+  deal(){return this.cards.pop();}
 }
